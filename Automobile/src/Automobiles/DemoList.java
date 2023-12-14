477bf74df316a1373969fcd0eae33ce72018fefe
Banking9 <bandaru2929@gmail.com>
2023-12-14 18:56:58 +0530

    Automobile Project First commit

---
+++ Automobile/src/Automobiles/DemoList.java
@@ -0,0 +1,31 @@
+package Automobiles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		List<Integer> numbers = new ArrayList<Integer>();
+		
+		numbers.add(200);
+		numbers.add(500);
+		numbers.add(400);
+		numbers.add(300);
+		numbers.add(900);
+		numbers.add(400);
+		
+		System.out.println(numbers);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
